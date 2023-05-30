6f3a8339824f8ff7f755ef435f6fab181fd4fdeb
luiggy_mamani <luiggy.mamani.12777190@usalesiana.edu.bo>
2023-05-29 23:39:05 -0400

    ci: :construction: logs to show responses

---
+++ src/main/java/org/example/view/Logs.java
@@ -0,0 +1,15 @@
+package org.example.view;
+
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
+
+public class Logs {
+
+    public void showIndicator() {
+        System.out.print(">> ");
+    }
+
+    public void showGeoJSON(MongoCursor<Document> locations) {
+        locations.forEachRemaining(System.out::println);
+    }
+}
